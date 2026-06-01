d00829552182a17921bd558c838af20c6d7a2af3
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2026-06-01 09:33:09 +0200

    Add WIP back to home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,24 @@
-import { createFileRoute } from "@tanstack/react-router"
-import { Button } from "@/components/ui/button"
+import { createFileRoute } from '@tanstack/react-router';
 
-export const Route = createFileRoute("/")({ component: App })
+export const Route = createFileRoute('/')({ component: App });
 
 function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+    <div className="flex min-h-screen items-center justify-center bg-white">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center gap-10 px-4 md:px-12">
+        <img
+          className=""
+          src="/tattou-wip.gif"
+          alt="Work In Progress tattou.ink"
+          width={300}
+          height={87}
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-[5px] text-black">
+            WIP
+          </h1>
         </div>
-      </div>
+      </main>
     </div>
-  )
+  );
 }
