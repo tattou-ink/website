a00725ddfbde8f87c3b9900fe428076c67e00761
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2026-06-01 12:09:35 +0200

    Improve privacy policy margins

--- src/routes/politique-de-confidentialite.tsx
+++ src/routes/politique-de-confidentialite.tsx
@@ -18,7 +18,7 @@ export const Route = createFileRoute('/politique-de-confidentialite')({
 function RouteComponent() {
   const { page } = Route.useLoaderData();
   return (
-    <main className='p-4 md:p-8 lg:p-16'>
+    <main className='p-4 md:p-8 lg:px-16'>
 
     <article>
       <Markdown content={page.content} className="prose" />
