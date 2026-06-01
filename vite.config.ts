1c54c7b766e6c4ed5293708896382a4298049a67
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2026-06-01 09:38:11 +0200

    Translate pathnames

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { translatedPathnames } from './i18n/lib';
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
@@ -15,6 +16,7 @@ const config = defineConfig({
       outputStructure: 'message-modules',
       cookieName: 'PARAGLIDE_LOCALE',
       strategy: ['url', 'cookie', 'preferredLanguage', 'baseLocale'],
+      urlPatterns: translatedPathnames,
     }),
     tailwindcss(),
     tanstackStart(),
