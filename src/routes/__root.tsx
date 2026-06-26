0a2cbf9c6eb5f6414864281762e6889873e0650c
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2026-06-26 09:16:10 +0200

    Fix tattou.ink casing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,7 @@ export const Route = createRootRoute({
     meta: [
       { charSet: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { title: 'Tattou.ink' },
+      { title: 'tattou.ink' },
     ],
     links: [{ rel: 'stylesheet', href: appCss }],
   }),
