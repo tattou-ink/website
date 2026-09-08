1f7f3a6c6247124951c40b82f9032b9986f7da5c
AlexandreS <32449369+AlexandreSi@users.noreply.github.com>
2026-09-08 20:55:27 +0200

    Try edgeSSR: true

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ const config = defineConfig({
       strategy: ['url', 'baseLocale'],
       urlPatterns: translatedPathnames,
     }),
-    netlify(),
+    netlify({ edgeSSR: true }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
